5b072fc9898b79ad681185d9ecdfb757e4d1a9f3
Shankarsan Ganai <shankershawn@gmail.com>
2018-08-12 17:24:22 +0530

    used eval for calculation

--- calculator.js
+++ calculator.js
@@ -3,7 +3,6 @@ var calculatorApp = angular.module("calculatorApp", []);
 calculatorApp.controller("calculatorController", function(){
     this.result = 0;
     this.chooseOperation = function(button){
-        console.log("inside chooseOperation");
         this.operation = button;
         this.clearErrorMessage();
     }
@@ -13,36 +12,20 @@ calculatorApp.controller("calculatorController", function(){
     }
 
     this.reset = function(){
-        this.input1 = "";
-        this.input2 = "";
-        this.operation = "";
-        this.result = "0";
+        this.input1 = this.input2 = this.operation = this.result = this.errorMessage = "";
     }
 
     this.operate = function(){
         if(undefined == this.input1 || undefined == this.input2 || "" == this.input1 || "" == this.input2){
             this.errorMessage = "Please enter the inputs!";
-            return false;
-        }
-        if(isNaN(this.input1) || isNaN(this.input2)){
+        }else if(isNaN(this.input1) || isNaN(this.input2)){
             this.errorMessage = "Please enter only numbers!";
-            return false;
-        }
-        if(this.operation == "+"){
-            this.result = Number(this.input1) + Number(this.input2);
-        }else if(this.operation == "-"){
-            this.result = Number(this.input1) - Number(this.input2);
-        }else if(this.operation == "*"){
-            this.result = Number(this.input1) * Number(this.input2);
-        }else if(this.operation == "/"){
-            if(Number(this.input2) == 0){
-                console.log("Error! Division by Zero!");
-                this.errorMessage = "You cannot divide by zero!";
-            }else{
-                this.result = Number(this.input1) / Number(this.input2);
-            }
-        }else{
+        }else if(undefined == this.operation || this.operation == ""){
             this.errorMessage = "Please enter an operand!";
+        }else if(this.operation == "/" && Number(this.input2) == 0){
+                this.errorMessage = "You cannot divide by zero!";
+        }else{
+            this.result = eval(this.input1 + this.operation + this.input2);
         }
     }
 });
