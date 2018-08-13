949650dd709b50ed56709fc36d1226d3975180f1
Shankarsan Ganai <shankershawn@gmail.com>
2018-08-14 01:41:30 +0530

    added ajax call for REST api

--- calculator.js
+++ calculator.js
@@ -1,5 +1,6 @@
-angular.module("calculatorApp", [])
-    .controller("calculatorController", function(){
+var calculatorModule = angular.module("calculatorApp", []);
+
+calculatorModule.controller("calculatorController", ["calculatorService", function(calculatorService){
         this.result = 0;
         this.operands = ["+", "-", "*", "/"];
         this.chooseOperation = function(button){
@@ -25,8 +26,46 @@ angular.module("calculatorApp", [])
                 this.errorMessage = "Please enter an operand!";
             }else if(this.operation == "/" && Number(this.input2) == 0){
                     this.errorMessage = "You cannot divide by zero!";
-            }else{
-                this.result = eval(this.input1 + this.operation + this.input2);
+            }else {
+                calculatorService.operate(this);
             }
         }
-    });
+    }]);
+
+calculatorModule.service("calculatorService", ["$http", function($http){
+    this.operate = function(controllerScope){
+        var serviceUrl = "";
+        if(controllerScope.operation == "+"){
+            serviceUrl = "http://192.168.43.137:5000/v1/calculator/addition"
+        }else if(controllerScope.operation == "-"){
+            serviceUrl = "http://192.168.43.137:5000/v1/calculator/subtraction"
+        }else if(controllerScope.operation == "*"){
+            serviceUrl = "http://192.168.43.137:5000/v1/calculator/multiplication"
+        }else if(controllerScope.operation == "/"){
+            serviceUrl = "http://192.168.43.137:5000/v1/calculator/division"
+        }
+        if(serviceUrl != ""){
+            $http({
+                url: serviceUrl,
+                method: "POST",
+                data: {
+                    "operand_1": controllerScope.input1,
+                    "operand_2": controllerScope.input2
+                },
+                headers: {"Authorization": "Basic c2hhbmthcnNhbjpjYXJjYXNz", "Content-Type": "application/json"}
+            }).then(function(response){
+                if(response != undefined){
+                    console.log(response.data);
+                    controllerScope.result = response.data.result;
+                }
+            }, function(response){
+                console.log("Service is temporarily unavailable.");
+                controllerScope.result = "0";
+                controllerScope.errorMessage = "Service is temporarily unavailable.";
+            });
+        }else{
+            controllerScope.result = "0";
+            controllerScope.errorMessage = "Invalid operand found!";
+        }
+    };
+}]);
